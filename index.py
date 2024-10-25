850cbfa999351fbe983c5bd644843a70ce388bb8
Fernando Torres <114715228+Fernandoat90@users.noreply.github.com>
2024-10-25 02:00:02 -0300

    Agregando funcionalidades al main

--- index.py
+++ index.py
@@ -5,6 +5,7 @@ opcion=0
 productos=[]
 precio=[]
 stock_min=[]
+stock_act=[]
 
 while(opcion!=9):
     
@@ -14,28 +15,41 @@ while(opcion!=9):
         opcion = int(input("Ingrese la opcion que desee: "))
             
     
-
+        #Mostrar
         if opcion==1:
 
-            Mostrar_productos(productos,precio)
+            Mostrar_productos(productos,precio,stock_act)
         
+        #Alta productos
         elif opcion==2:
 
             prod_alta=Agregar_producto()
+            
             productos.append(prod_alta[0])
             precio.append(prod_alta[1])
             stock_min.append(prod_alta[2])
+            stock_act.append(prod_alta[3])
+            
+            print("\nProducto Agregado.")
         
+        #Eliminar Productos
         elif opcion==3:
             
-            Eliminar_producto(productos,precio,stock_min)
+            Eliminar_producto(productos,precio,stock_min,stock_act)
         
+        #Modificar Productos
         elif opcion==4:
-            Modificar_producto(productos,precio,stock_min)
+            Modificar_producto(productos,precio,stock_min,stock_act)
         
+        #Buscar Productos
         elif opcion==5:
-            Buscar_producto(productos,precio,stock_min)
-
+            Buscar_producto(productos,precio,stock_min,stock_act)
+        
+        #Productos bajo stock
+        elif opcion==6:
+            
+            Bajo_stk(productos,precio,stock_min,stock_act)
+        
         elif opcion==9:
             print("\n\tSaliendo del programa")
         
